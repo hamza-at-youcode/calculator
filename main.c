8ec139a0bcac515ba972472d1aa2fca953c79e87
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-12 12:31:31 +0100

    Impliment functions

--- main.c
+++ main.c
@@ -2,6 +2,52 @@
 #include <conio.h>
 #include<stdlib.h>
 
+float add(){
+    float a,s = 0;
+    printf("Enter 0 to end!\n");
+    do
+    {
+        printf("> ");
+        scanf("%f",&a);
+        s+=a;
+    } while (a!=0);
+    return s;
+}
+
+float sub(){
+    float a,s = 0;
+    printf("Enter 0 to end!\n");
+    do
+    {
+        printf("> ");
+        scanf("%f",&a);
+        s-=a;
+    } while (a!=0);
+    return s;
+}
+
+float _div(){
+    float a,b;
+    printf("Enter the first numbre: ");
+    scanf("%f",&a);
+    printf("Enter the second numbre: ");
+    scanf("%f",&b);
+    if (b == 0) printf("\nTake a coffee and try a try again!");
+    else printf("Result: %0.2f / %0.2f = %0.2f",a,b,a/b);
+}
+
+float mul(){
+    float a,m = 1;
+    printf("Enter 1 to end!\n");
+    do
+    {
+        printf("> ");
+        scanf("%f",&a);
+        m*=a;
+    } while (a!=1);
+    return m;
+}
+
 int main(){
     printf("\nWelcome to youCode calc\n");
     char ch;
@@ -19,33 +65,16 @@ int main(){
         switch (ch)
         {
         case '+':
-            printf("Enter the first numbre: ");
-            scanf("%f",&a);
-            printf("Enter the second numbre: ");
-            scanf("%f",&b);
-            printf("Result: %0.2f + %0.2f = %0.2f",a,b,a+b);
+            printf("S = %0.2f",add());
             break;
         case '-':
-            printf("Enter the first numbre: ");
-            scanf("%f",&a);
-            printf("Enter the second numbre: ");
-            scanf("%f",&b);
-            printf("Result: %0.2f - %0.2f = %0.2f",a,b,a-b);
+            printf("S = %0.2f",sub());
             break;
         case '*':
-            printf("Enter the first numbre: ");
-            scanf("%f",&a);
-            printf("Enter the second numbre: ");
-            scanf("%f",&b);
-            printf("Result: %0.2f * %0.2f = %0.2f",a,b,a*b);
+            printf("M = %0.2f",mul());
             break;
         case '/':
-            printf("Enter the first numbre: ");
-            scanf("%f",&a);
-            printf("Enter the second numbre: ");
-            scanf("%f",&b);
-            if (b == 0) printf("\nTake a coffee and try a try again!");
-            else printf("Result: %0.2f / %0.2f = %0.2f",a,b,a/b);
+            _div();
             break;
         case '0':exit(0);
         default:
